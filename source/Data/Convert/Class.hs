fda19db6420d0fcfbdab3b80b4421fbdbb31222c
Julian Brunner <julian.brunner@gmail.com>
2025-03-19 22:40:33 +0100

    use additional type parameter

--- source/Data/Convert/Class.hs
+++ source/Data/Convert/Class.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FunctionalDependencies #-}
 
 module Data.Convert.Class
 (
@@ -8,36 +8,30 @@ module Data.Convert.Class
 where
 
 import Control.Exception
-import Data.Kind
 import Data.Bifunctor
 import Data.Convert.Tools
 import GHC.Stack
 
-class Convert a b where
-    convert :: a -> b
--- TODO: instead of indexed family, use additional type parameter
-class Partial a b where
-    type Failure a b :: Type
-    type Failure a b = ()
-    partial :: a -> Either (Failure a b) b
+class Convert a b where convert :: a -> b
+class Partial e a b | a b -> e where partial :: a -> Either e b
 
 into :: forall b a. Convert a b => a -> b
 into = convert
 
-partialUndefined :: HasCallStack => Failure a b ~ () => Partial a b => a -> b
+partialUndefined :: HasCallStack => Partial () a b => a -> b
 partialUndefined = either undefined id . partial
 
-partialError :: HasCallStack => Failure a b ~ String => Partial a b => a -> b
+partialError :: HasCallStack => Partial String a b => a -> b
 partialError = either error id . partial
 
-partialThrow :: HasCallStack => Exception (Failure a b) => Partial a b => a -> b
+partialThrow :: HasCallStack => Exception e => Partial e a b => a -> b
 partialThrow = either throw id . partial
 
-partialMaybe :: Failure a b ~ () => Partial a b => a -> Maybe b
+partialMaybe :: Partial () a b => a -> Maybe b
 partialMaybe = eitherToMaybe . partial
 
-partialString :: Failure a b ~ String => Partial a b => a -> Either String b
+partialString :: Partial String a b => a -> Either String b
 partialString = partial
 
-partialDisplay :: Exception (Failure a b) => Partial a b => a -> Either String b
+partialDisplay :: Exception e => Partial e a b => a -> Either String b
 partialDisplay = first displayException . partial
--- source/Data/Convert/Instance.hs
+++ source/Data/Convert/Instance.hs
@@ -1,5 +1,4 @@
 {-# OPTIONS_GHC -Wno-orphans #-}
-{-# LANGUAGE TypeFamilies #-}
 
 module Data.Convert.Instance () where
 
@@ -11,43 +10,42 @@ import Numeric.Natural
 import Data.Word
 import Data.Bits
 import Data.ByteString qualified as B
-import Data.Text.Encoding.Error qualified as T
 import Data.Int
 import Text.Printf
 import Data.Convert.Tools
+import Data.Text.Encoding.Error
 
 instance Convert Bool Natural where
     convert False = 0
     convert True = 1
-instance Partial Natural Bool where
-    type Failure Natural Bool = String
+instance Partial String Natural Bool where
     partial 0 = Right False
     partial 1 = Right True
     partial n = Left $ printf "expected 0 or 1, got %d" n
 
 instance Convert Word8 Natural where convert = fromIntegral
-instance Partial Natural Word8 where type Failure Natural Word8 = String; partial = maybeToEitherMessage toIntegralSized
+instance Partial String Natural Word8 where partial = maybeToEitherMessage toIntegralSized
 
 instance Convert Word16 Natural where convert = fromIntegral
-instance Partial Natural Word16 where type Failure Natural Word16 = String; partial = maybeToEitherMessage toIntegralSized
+instance Partial String Natural Word16 where partial = maybeToEitherMessage toIntegralSized
 
 instance Convert Word32 Natural where convert = fromIntegral
-instance Partial Natural Word32 where type Failure Natural Word32 = String; partial = maybeToEitherMessage toIntegralSized
+instance Partial String Natural Word32 where partial = maybeToEitherMessage toIntegralSized
 
 instance Convert Word64 Natural where convert = fromIntegral
-instance Partial Natural Word64 where type Failure Natural Word64 = String; partial = maybeToEitherMessage toIntegralSized
+instance Partial String Natural Word64 where partial = maybeToEitherMessage toIntegralSized
 
 instance Convert Int Int64 where convert = fromIntegral
-instance Partial Int64 Int where type Failure Int64 Int = String; partial = maybeToEitherMessage toIntegralSized
+instance Partial String Int64 Int where partial = maybeToEitherMessage toIntegralSized
 
-instance Partial Int Word8 where type Failure Int Word8 = String; partial = maybeToEitherMessage toIntegralSized
-instance Partial Word8 Int where type Failure Word8 Int = String; partial = maybeToEitherMessage toIntegralSized
+instance Partial String Int Word8 where partial = maybeToEitherMessage toIntegralSized
+instance Partial String Word8 Int where partial = maybeToEitherMessage toIntegralSized
 
-instance Partial Int Natural where type Failure Int Natural = String; partial = maybeToEitherMessage toIntegralSized
-instance Partial Natural Int where type Failure Natural Int = String; partial = maybeToEitherMessage toIntegralSized
+instance Partial String Int Natural where partial = maybeToEitherMessage toIntegralSized
+instance Partial String Natural Int where partial = maybeToEitherMessage toIntegralSized
 
 instance Convert Natural Integer where convert = toInteger
-instance Partial Integer Natural where type Failure Integer Natural = String; partial = maybeToEitherMessage toIntegralSized
+instance Partial String Integer Natural where partial = maybeToEitherMessage toIntegralSized
 
 instance Convert ByteString Natural where
     convert = flip B.foldl' 0 $ \ n a -> n !<<. 8 .|. fromIntegral a
@@ -57,6 +55,4 @@ instance Convert Natural ByteString where
         go n = Just (fromIntegral n, n !>>. 8)
 
 instance Convert Text ByteString where convert = T.encodeUtf8
-instance Partial ByteString Text where
-    type Failure ByteString Text = T.UnicodeException
-    partial = T.decodeUtf8'
+instance Partial UnicodeException ByteString Text where partial = T.decodeUtf8'
