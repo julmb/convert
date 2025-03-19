4883fded1c45431a0b4937251ab1bccfb0c9ef2e
Julian Brunner <julian.brunner@gmail.com>
2025-03-19 22:32:04 +0100

    add more invocation helpers

--- source/Data/Convert/Class.hs
+++ source/Data/Convert/Class.hs
@@ -1,6 +1,11 @@
 {-# LANGUAGE TypeFamilies #-}
 
-module Data.Convert.Class (Convert (..), Partial (..), into, partialThrow, partialMaybe, partialDisplay) where
+module Data.Convert.Class
+(
+    Convert (..), Partial (..), into,
+    partialUndefined, partialError, partialThrow, partialMaybe, partialString, partialDisplay
+)
+where
 
 import Control.Exception
 import Data.Kind
@@ -10,6 +15,7 @@ import GHC.Stack
 
 class Convert a b where
     convert :: a -> b
+-- TODO: instead of indexed family, use additional type parameter
 class Partial a b where
     type Failure a b :: Type
     type Failure a b = ()
@@ -18,11 +24,20 @@ class Partial a b where
 into :: forall b a. Convert a b => a -> b
 into = convert
 
+partialUndefined :: HasCallStack => Failure a b ~ () => Partial a b => a -> b
+partialUndefined = either undefined id . partial
+
+partialError :: HasCallStack => Failure a b ~ String => Partial a b => a -> b
+partialError = either error id . partial
+
 partialThrow :: HasCallStack => Exception (Failure a b) => Partial a b => a -> b
 partialThrow = either throw id . partial
 
 partialMaybe :: Failure a b ~ () => Partial a b => a -> Maybe b
 partialMaybe = eitherToMaybe . partial
+
+partialString :: Failure a b ~ String => Partial a b => a -> Either String b
+partialString = partial
 
 partialDisplay :: Exception (Failure a b) => Partial a b => a -> Either String b
 partialDisplay = first displayException . partial
