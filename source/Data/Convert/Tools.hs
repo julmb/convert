4af9bd5178642680a6d3adc43d2949c68c6ced3d
Julian Brunner <julian.brunner@gmail.com>
2025-03-19 22:31:33 +0100

    add reflection message tool

--- source/Data/Convert/Tools.hs
+++ source/Data/Convert/Tools.hs
@@ -1,7 +1,14 @@
-module Data.Convert.Tools (maybeToEither, eitherToMaybe) where
+module Data.Convert.Tools (maybeToEither, eitherToMaybe, maybeToEitherMessage) where
+
+import Type.Reflection
+import Text.Printf
 
 maybeToEither :: Maybe a -> Either () a
 maybeToEither = maybe (Left ()) Right
 
 eitherToMaybe :: Either () a -> Maybe a
 eitherToMaybe = either (\ () -> Nothing) Just
+
+maybeToEitherMessage :: forall a b. Typeable a => Typeable b => Show a => (a -> Maybe b) -> (a -> Either String b)
+maybeToEitherMessage f a = maybe (Left message) Right $ f a where
+    message = printf "could not convert %s :: %s to %s" (show a) (show $ typeRep @a) (show $ typeRep @b)
