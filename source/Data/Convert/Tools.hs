bd23b54a65393dc96f3399c8ec9f659af5958129
Julian Brunner <julian.brunner@gmail.com>
2025-03-20 18:44:58 +0100

    cleanup

--- source/Data/Convert/Tools.hs
+++ source/Data/Convert/Tools.hs
@@ -1,14 +1,7 @@
-module Data.Convert.Tools (maybeToEither, eitherToMaybe, maybeToEitherMessage) where
-
-import Type.Reflection
-import Text.Printf
+module Data.Convert.Tools (maybeToEither, eitherToMaybe) where
 
 maybeToEither :: Maybe a -> Either () a
 maybeToEither = maybe (Left ()) Right
 
 eitherToMaybe :: Either () a -> Maybe a
 eitherToMaybe = either (\ () -> Nothing) Just
-
-maybeToEitherMessage :: forall a b. Typeable a => Typeable b => Show a => (a -> Maybe b) -> (a -> Either String b)
-maybeToEitherMessage f a = maybe (Left message) Right $ f a where
-    message = printf "could not convert %s from %s to %s" (show a) (show $ typeRep @a) (show $ typeRep @b)
