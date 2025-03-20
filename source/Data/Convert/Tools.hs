f337084681343e9fc7b6b26ab1ba58059c380857
Julian Brunner <julian.brunner@gmail.com>
2025-03-20 04:37:27 +0100

    improve message

--- source/Data/Convert/Tools.hs
+++ source/Data/Convert/Tools.hs
@@ -11,4 +11,4 @@ eitherToMaybe = either (\ () -> Nothing) Just
 
 maybeToEitherMessage :: forall a b. Typeable a => Typeable b => Show a => (a -> Maybe b) -> (a -> Either String b)
 maybeToEitherMessage f a = maybe (Left message) Right $ f a where
-    message = printf "could not convert %s :: %s to %s" (show a) (show $ typeRep @a) (show $ typeRep @b)
+    message = printf "could not convert %s from %s to %s" (show a) (show $ typeRep @a) (show $ typeRep @b)
