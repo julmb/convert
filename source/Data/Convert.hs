a117859984ecea26cdc0742d77d4d6dcdbd2fb19
Julian Brunner <julian.brunner@gmail.com>
2025-06-18 20:28:33 +0200

    merge modules

--- source/Data/Convert.hs
+++ source/Data/Convert.hs
@@ -1,7 +1,6 @@
-module Data.Convert (module Data.Convert.Tools, module Data.Convert.Class, module Data.Convert.Consume) where
+module Data.Convert (module Data.Convert.Tools, module Data.Convert.Class) where
 
 import Data.Convert.Tools
 import Data.Convert.Class
-import Data.Convert.Consume
 import Data.Convert.Numeric ()
 import Data.Convert.Instance ()
